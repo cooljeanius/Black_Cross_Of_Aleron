1fe5ae6054b7c806108918cdb26d0f9bc3b9192f
Celtic Minstrel <celtic.minstrel.ca@some.place>
2018-08-14 21:14:25 -0400

    Whenever you get some potions, show a summary of what exactly you got

---
+++ lua/potions.lua
@@ -0,0 +1,25 @@
+
+local _ = wesnoth.textdomain "wesnoth-celmin-bcoa"
+
+local haste_total = wml.variables.haste_potion
+local healing_total = wml.variables.healing_potion
+local haste_new = haste_total - wml.variables.haste_potion_old
+local healing_new = healing_total - wml.variables.healing_potion_old
+
+local haste_detail = wesnoth.format(_("another haste potion","$potions more haste potions",haste_new), {potions = haste_new})
+local healing_detail = wesnoth.format(_("another healing potion","$potions more healing potions",healing_new), {potions = healing_new})
+
+local haste_total = wesnoth.format(_("just one haste potion", "$potions haste potions", haste_total), {potions = haste_total})
+local healing_total = wesnoth.format(_("just one healing potion", "$potions healing potions", healing_total), {potions = healing_total})
+
+local open_green = "<span color='darkgreen' font_weight='bold'>"
+local open_orange = "<span color='orange' font_weight='bold'>"
+local close = "</span>"
+
+haste_detail = open_orange .. haste_detail .. close
+haste_total = open_orange .. haste_total .. close
+healing_detail = open_green .. healing_detail .. close
+healing_total = open_green .. healing_total .. close
+
+wml.variables.potion_details = wesnoth.format_conjunct_list('', {haste_detail, healing_detail})
+wml.variables.potion_totals = wesnoth.format_conjunct_list('', {haste_total, healing_total})
