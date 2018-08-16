fbfcf457d9978cfa459ada8a65efe7dd4a041629
Celtic Minstrel <celtic.minstrel.ca@some.place>
2018-08-15 23:48:03 -0400

    Scenario 3: Fix incorrect potions report when searching the shipwreck

--- lua/potions.lua
+++ lua/potions.lua
@@ -3,8 +3,8 @@ local _ = wesnoth.textdomain "wesnoth-celmin-bcoa"
 
 local haste_total = wml.variables.haste_potion
 local healing_total = wml.variables.healing_potion
-local haste_new = haste_total - (wml.variables.haste_potion_old or 0)
-local healing_new = healing_total - (wml.variables.healing_potion_old or 0)
+local haste_new = haste_total - (wml.variables.haste_potion_old or haste_total)
+local healing_new = healing_total - (wml.variables.healing_potion_old or healing_total)
 
 local haste_detail = wesnoth.format(_("another haste potion","$potions more haste potions",haste_new), {potions = haste_new})
 local healing_detail = wesnoth.format(_("another healing potion","$potions more healing potions",healing_new), {potions = healing_new})
@@ -21,5 +21,9 @@ haste_total = open_orange .. haste_total .. close
 healing_detail = open_green .. healing_detail .. close
 healing_total = open_green .. healing_total .. close
 
-wml.variables.potion_details = wesnoth.format_conjunct_list('', {haste_detail, healing_detail})
+local new_list = {}
+if haste_new > 0 then table.insert(new_list, haste_detail) end
+if healing_new > 0 then table.insert(new_list, healing_detail) end
+
+wml.variables.potion_details = wesnoth.format_conjunct_list(_'no more potions', new_list)
 wml.variables.potion_totals = wesnoth.format_conjunct_list('', {haste_total, healing_total})
